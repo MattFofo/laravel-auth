a942499157098e2110e1c15bf34a4d339721bbb2
MattFofo <matteo.segattini2@gmail.com>
2022-05-18 15:40:38 +0200

    model

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'slug'
+    ];
+
+    //metodo per generare slug unico da stringa
+    static public function generateSlug($fromThisStr) {
+        $baseSlug = Str::slug($fromThisStr, '-');
+        $slug = $baseSlug;
+        $i = 1;
+
+        while (self::where('slug', $slug)->first()) {
+            $slug = "$baseSlug-$i";
+            $i++;
+        }
+        return $slug;
+    }
 }
