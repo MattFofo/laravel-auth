9798f04dd908425fc6b5205bc1563f8498840b7a
MattFofo <matteo.segattini2@gmail.com>
2022-05-19 14:36:10 +0200

    posts.show + posts.index + route fix

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,9 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($posts as $post)
-        <h2>{{ $post->title }}</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Created_at</th>
+                <th scope="col">Updated_at</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
 
-    @endforeach
+                    <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->slug }}</td>
+                    <td>{{ date('d/m/Y', strtotime($post->created_at)) }}</td>
+                    <td>{{ date('d/m/Y', strtotime($post->updated_at)) }}</td>
+
+                    <td><a href="{{ route('admin.posts.show', $post) }}">SHOW</a></td>
+                    <td><a href="">EDIT</a></td>
+                    <td><button>DELETE</button></td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {{ $posts->links() }}
 
 @endsection
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>{{ $post->title }}</h1>
+                <small>{{ date('d/m/Y' , strtotime($post->created_at)) }}</small>
+
+                {{-- post updated --}}
+                @if ($post->created_at != $post->updated_at)
+                    <small>{{ date('d/m/Y' , strtotime($post->updated_at)) }}</small>
+                @endif
+                <p>{{ $post->content }}</p>
+            </div>
+        </div>
+    </div>
+@endsection
