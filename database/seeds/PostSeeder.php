340e01bf25ec53621603ec5436f0feae7108b3a3
MattFofo <matteo.segattini2@gmail.com>
2022-05-20 14:33:41 +0200

    popolata tabella users + aggiunta foreign key a tabella posts

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 use Faker\Generator as Faker;
-
 use App\Post;
+use App\User;
 
 class PostSeeder extends Seeder
 {
@@ -19,6 +18,7 @@ class PostSeeder extends Seeder
             $title = $faker->words(rand(2, 5), true);
 
             $postData = [
+                'user_id'   => User::inRandomOrder()->first()->id,
                 'title'     => $title,
                 'content'   => $faker->text(rand(200, 1000)),
                 'slug'      => Post::generateSlug($title)
